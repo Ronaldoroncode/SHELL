e2db111ac7a208aa0eea12abec8356b064d6cccf
ronaldoroncode <roncodeit@gmail.com>
2022-09-03 09:41:58 -0300

    script #1 nomedamusica

---
+++ nomedamusica.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Gênero: $1"
+echo "Artista: $2"
+echo "Nome da música: $3"
+echo " "
+echo "Todos os parâmetros:"
+echo "$*"
+echo "Quantidade de parâmetros: $#"
+echo "Comando para executar:"
+echo "$0"
